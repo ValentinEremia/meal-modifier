945009691753f1a1b3473e8c36137e807f180a47
Eremia Valentin <valentineremia@yahoo.com>
2025-06-11 20:41:41 +0300

    first commit

--- app/api/modify/route.ts
+++ app/api/modify/route.ts
@@ -1,14 +1,19 @@
+import { readFileSync } from 'fs';
 import { google } from 'googleapis';
 import { NextRequest, NextResponse } from 'next/server';
-import { readFileSync } from 'fs';
 import path from 'path';
 
+// Accesează variabilele de mediu
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
-const SHEET_ID = '1mBBLrQTFvstLCCBV7aFs9xo6MbE_HEoikQwZrdttyDc'; // din URL
-const SHEET_NAME = 'MENIU 19-27 SEPTEMBRIE 2025';
+const SHEET_ID = process.env.SHEET_ID; // din .env.local
+const SHEET_NAME = process.env.SHEET_NAME; // din .env.local
 
+// Verifică dacă este mediu de dezvoltare și încarcă credențialele din fișierul JSON sau din variabilele de mediu
+const credentials = process.env.NODE_ENV === 'development'
+  ? JSON.parse(readFileSync(path.join(process.cwd(), 'credentials/service-account.json'), 'utf-8'))
+  : JSON.parse(process.env.GOOGLE_CREDENTIALS || '{}'); // din .env.local
 const auth = new google.auth.GoogleAuth({
-  credentials: JSON.parse(readFileSync(path.join(process.cwd(), 'credentials/service-account.json'), 'utf-8')),
+  credentials,
   scopes: SCOPES,
 });
 
@@ -40,31 +45,20 @@ export async function POST(req: NextRequest) {
     return NextResponse.json({ error: '⚠️ Numele nu a fost găsit.' }, { status: 404 });
   }
 
-  // let colIndex = -1;
-  // for (let i = 0; i < dateRow.length; i++) {
-  //   const d = new Date(dateRow[i]);
-  //   d.setHours(0, 0, 0, 0);
-  //   if (d.getTime() === dateObj.getTime() && (mealRow[i] || '').toLowerCase().trim() === meal.toLowerCase().trim()) {
-  //     colIndex = i;
-  //     break;
-  //   }
-  // }
-
   let colIndex = -1;
-for (let i = 0; i < dateRow.length; i++) {
-  const [day, month, year] = (dateRow[i] || '').split('-');
-  if (!day || !month || !year) continue;
-  const d = new Date(`${year}-${month}-${day}`);
-  d.setHours(0, 0, 0, 0);
-  if (
-    d.getTime() === dateObj.getTime() &&
-    (mealRow[i] || '').toLowerCase().trim() === meal.toLowerCase().trim()
-  ) {
-    colIndex = i;
-    break;
+  for (let i = 0; i < dateRow.length; i++) {
+    const [day, month, year] = (dateRow[i] || '').split('-');
+    if (!day || !month || !year) continue;
+    const d = new Date(`${year}-${month}-${day}`);
+    d.setHours(0, 0, 0, 0);
+    if (
+      d.getTime() === dateObj.getTime() &&
+      (mealRow[i] || '').toLowerCase().trim() === meal.toLowerCase().trim()
+    ) {
+      colIndex = i;
+      break;
+    }
   }
-}
-
 
   if (colIndex === -1) {
     return NextResponse.json({ error: '⚠️ Masa nu a fost găsită pentru acea zi.' }, { status: 404 });
